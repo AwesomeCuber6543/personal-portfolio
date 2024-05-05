2b352b681515177b2cd2fc8573ae7ef32acd8eae
Yahia Salman <yahia.salman.04@gmail.com>
2024-05-04 21:10:31 -0400

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 import Navbar from './Navbar';
+import Home from './Home';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
+      <div className='Home'>
+        <Home/>
+      </div>
       <div className="Navbar">
         <Navbar/>
       </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
-    return(
-        <nav className='nav'>
-            <a href='/' className='site-title'>Yahia Salman</a>
+    const [isOpen, setIsOpen] = useState(false);
 
-            <ul>
-                <li><a href='/Education'>Education</a></li>
-                <li>Experience</li>
-                <li>Projects</li>
-                <li>Accomplishments</li>
-                <li>Research</li>
-            </ul>
-        </nav>
+    const toggleSidebar = () => setIsOpen(!isOpen);
+    const closeSidebar = () => setIsOpen(false)
+
+    return (
+        <div>
+            <button onClick={toggleSidebar} className="menu-button">
+                {isOpen ? 'Close' : 'Menu'}
+            </button>
+            <nav className={`nav ${isOpen ? 'open' : ''}`}>
+                <a href='/' className='site-title'>Yahia Salman</a>
+                <ul>
+                    <li><a href='/Accomplishments'>Accomplishments</a></li>
+                    <li><a href='/Research'>Research</a></li>
+                    <li><a href='/Projects'>Projects</a></li>
+                    <li><a href='/Experience'>Experience</a></li>
+                    <li><a href='/Education'>Education</a></li>
+                </ul>
+            </nav>
+        </div>
     );
-}
+};
 
 export default Navbar
